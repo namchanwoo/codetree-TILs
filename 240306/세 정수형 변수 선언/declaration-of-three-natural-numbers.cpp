ae08812f3670613690ff3ac06eb4a2fac30b144a
namchanwoo <58328281+namchanwoo@users.noreply.github.com>
2024-03-06 01:09:32 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int a = 7;
+    int b= 23;
+    int c = a +b;
+
+    cout << a << " + " << b << " = " << c;
+
+	return 0;
+
+}
