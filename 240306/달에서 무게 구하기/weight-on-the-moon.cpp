99a35296a5c2147f8f8c9125e36db08d9da76d59
namchanwoo <58328281+namchanwoo@users.noreply.github.com>
2024-03-06 02:33:41 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/달에서 무게 구하기/weight-on-the-moon.cpp
+++ 240306/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main() {
 
-	cout << fixed;
-    cout.precision(6);
+	
 
 	double a = 13.0f;
     double b = 0.165000;
     double c = a*b;
 
+    cout << fixed;
+    cout.precision(6);
 	
 	cout << a <<"*"<<b<<"="<<c;
 
