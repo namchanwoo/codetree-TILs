b4958b69edd699c764b0a89b5fe4ce1a92aab154
namchanwoo <58328281+namchanwoo@users.noreply.github.com>
2024-03-08 08:34:56 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/데이터 교환/exchange-data.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5, b = 6,c=7;
+    int temp;
+
+    temp = b;
+    b = a;
+    a = c;
+    c = temp;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c<<endl;
+
+    return 0;
+
+}
